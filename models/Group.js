e58a5fa70e9228a3a76001a32f4d137a2517a346
Anirudh <anirudhatodaria11@gmail.com>
2020-11-02 19:22:38 +0530

    added group and message models

---
+++ models/Group.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  type: {
+    type: String,
+    enum: ['personal', 'group'],
+    default: 'personal'
+  },
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }]
+});
+
+const Group = mongoose.model('group', groupSchema);
+
+module.exports = Group;
