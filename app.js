55f4daf960757131b202de3766f8913f54f03077
Anirudh <anirudhatodaria11@gmail.com>
2020-11-06 09:27:42 +0530

    basic structure with socketio

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const cors = require('cors');
 
 //ROUTE IMPORTS
 const userRoute = require('./routes/userRoute.js');
+const { Socket } = require('dgram');
+const { Console } = require('console');
 
 const port = process.env.PORT || 4004;
 const app = express();
+
+const server = require('http').createServer(app);
+const io = require('./socket').init(server);
 
 //MIDDLEWARES
 app.use(express.json());
@@ -23,7 +28,7 @@ mongoose.connect(process.env.DB_URL, {
   useCreateIndex: true,
   useFindAndModify: false
 }).then(response => {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server started on ${port}`);
   });
 }).catch(err => {
